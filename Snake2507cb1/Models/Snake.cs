e578e00ba31991134b595749025302c6b4859b1b
FIoLka6 <egorka02087@gmail.com>
2026-04-14 09:16:35 +0700

    Update Snake.cs

--- Snake2507cb1/Models/Snake.cs
+++ Snake2507cb1/Models/Snake.cs
@@ -5,43 +5,19 @@
         public int Id;
         public Coordinats Coordinats = new Coordinats(2, 2);
         public Vector Vector = new Vector(0, 1);
-
-        // Вспомогательное поле для отложенной смены вектора
-        private Vector _nextVector = null;
-
         public Snake()
         {
         }
-
         public override string ToString()
         {
             return $"{Coordinats.ToString()}\tvector:{Vector.ToString()}";
         }
 
-        // Изменить вектор, если поворот допустим
-        public void ChangeVector(Vector newVector)
-        {
-            // Проверяем, не разворот ли это на 180 градусов
-            if (Vector.CanChangeTo(newVector))
-            {
-                _nextVector = newVector;
-            }
-            // Если поворот недопустим — игнорируем нажатие
-        }
-
-        // Изменить координату по текущему вектору
+        //todo методы изменения координат
         public void Move()
         {
-            // Если есть отложенный вектор — применяем его
-            if (_nextVector != null)
-            {
-                Vector = _nextVector;
-                _nextVector = null;
-            }
-
-            // Новая позиция = старая + вектор
-            // Используем метод Add из класса Coordinats
-            Coordinats = Coordinats.Add(Vector.Coordinats);
+            Coordinats.X += Vector.Coordinats.X;
+            Coordinats.Y += Vector.Coordinats.Y;
         }
     }
 }
